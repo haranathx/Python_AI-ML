aa12129d1c03aa87fa7d202380a67a2fecd97c0e
haranathx <haranathx@gmail.com>
2025-10-13 17:18:31 +0530

    PY

---
+++ for_loop.py
@@ -0,0 +1,24 @@
+# # color/i/n is a variable
+
+# name="Haranath Mondal"
+
+# for i in name:
+#     print(i)
+
+
+# # colors is a list
+# colors= ["Red","Green","Blue","Cyan","Magenta","Yellow","Key(Black)"]
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+
+# for loop in range
+
+# for n in range(5):
+#     print(n+1)
+
+for n in range(1,9):
+    print(n)
