378dd1a79b74938281ee9214225832f1f772b8aa
haranathx <haranathx@gmail.com>
2025-10-16 08:38:49 +0530

    PY

---
+++ 6_List.py
@@ -0,0 +1,36 @@
+L = [3,4,5,6,0,1,2,7,8,9]
+
+# print(L)
+
+# print(type(L))
+
+# print(L[10])
+
+
+# list methods
+
+# let add 28 in the lis
+L.append(28)
+
+print(L)
+
+# reverse the list
+L.reverse()
+
+# sort the list
+L.sort()
+print(L)
+# sort the list descending order
+L.sort(reverse=True)
+
+L.insert(1,899)
+print(L)
+
+M = [500,600,700,1000]
+
+K = L + M
+
+# print(K)
+# L.extend(M)
+print(L)
+
